b8d7e488912aefe8f78d3b0a892e9b24ec4b3272
Liberbell <liberbell@gmail.com>
2020-04-26 12:56:48 +0900

    concurrentmodule2

--- concurrentmodule.py
+++ concurrentmodule.py
@@ -1,3 +1,7 @@
 # from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import ThreadPoolExecutor
 import time
+
+def return_after_n_secs(n, message):
+    time.sleep(n)
+    return message
