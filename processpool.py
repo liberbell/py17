1fa023848daa4db5884a23f8dd49be9f5af2bfa3
Liberbell <liberbell@gmail.com>
2020-04-26 12:35:30 +0900

    processpool2

--- processpool.py
+++ processpool.py
@@ -36,13 +36,23 @@ def sum_square(number):
         s += i * i
     return s
 
-def sum_square_with_map(numbers):
+def sum_square_with_mp(numbers):
     start_time = time.time()
     p = Pool()
     result = p.map(sum_square, numbers)
 
     p.close()
     p.join()
+
+    total_time = time.time() - start_time
+    print("Processing %d numbers took %.2f seconds using multiprocessing" % (len(numbers), total_time))
+
+    def sum_square_without_mp(numbers):
+    start_time = time.time()
+
+    result = []
+    for i in numbers:
+        result.append(sum_square(i))
 
     total_time = time.time() - start_time
     print("Processing %d numbers took %.2f seconds using multiprocessing" % (len(numbers), total_time))
