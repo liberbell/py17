7abea225ec6ad84426d155d280d73103de5fe536
Liberbell <liberbell@gmail.com>
2020-04-27 14:43:21 +0900

    concurrentmodule7

--- concurrentmodule.py
+++ concurrentmodule.py
@@ -6,9 +6,13 @@ def return_after_n_secs(n, message):
     time.sleep(n)
     return message
 
-pool = ThreadPoolExecutor(3)
-submitted_job = pool.submit(return_after_n_secs, 10, "Hello")
+# pool = ThreadPoolExecutor(3)
+# submitted_job = pool.submit(return_after_n_secs, 10, "Hello")
 
-print(submitted_job.done())
-print(submitted_job.result())
-print(submitted_job.done())
+# print(submitted_job.done())
+# print(submitted_job.result())
+# print(submitted_job.done())
+
+num_list = [2, 3, 4, 5, 6]
+def cal_square(x):
+    return x * x
