7f423ea1fffa72dce6dbe60e5854ea0ed763c2cf
Liberbell <liberbell@gmail.com>
2020-04-28 16:23:49 +0900

    compconcurrent4

--- compconcurrent.py
+++ compconcurrent.py
@@ -1,3 +1,8 @@
 from concurrent.futures import ProcessPoolExecutor
 from concurrent.futures import as_completed
 import urllib.request
+
+url_list = ["https://loonycorn.com/",
+            "https://reuters.com/",
+            "https://wwf.panda.org/",
+            "https://en.unesco.org/"]
