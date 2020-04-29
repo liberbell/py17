3fbe6ce63cfdaaa9740db47c5e0617262629348d
Liberbell <liberbell@gmail.com>
2020-04-29 18:54:33 +0900

    compconcurrent1

--- compconcurrent.py
+++ compconcurrent.py
@@ -1,4 +1,5 @@
 from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import as_completed
 import urllib.request
 import time
@@ -16,6 +17,20 @@ def main_processpool():
     start = time.time()
 
     with ProcessPoolExecutor(max_workers=7) as executor:
+        future_to_page = {executor.submit(url_loader, url, 10):
+                          url for url in url_list}
+
+        for future in as_completed(future_to_page):
+            url = future_to_page[future]
+            result = future.result()
+            print("The page %r is %d bytes." % (url, len(result)))
+    
+    print("Total time taken:", time.time() - start)
+
+def main_threadpool():
+    start = time.time()
+
+    with ThreadPoolExecutor(max_workers=7) as executor:
         future_to_page = {executor.submit(url_loader, url, 10):
                           url for url in url_list}
 
