fa0328629cc05cb3c53cff75730b9cc3e3800ee8
Liberbell <liberbell@gmail.com>
2020-04-29 19:00:08 +0900

    threadprocess1

---
+++ threadprocess.py
@@ -0,0 +1,5 @@
+from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
+import urllib.request
+import time
