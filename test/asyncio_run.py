35070b5c2d2df8214581348dc52021f449de21fe
Liberbell <liberbell@gmail.com>
2020-04-30 17:38:06 +0900

    asyncio_run3

--- test/asyncio_run.py
+++ test/asyncio_run.py
@@ -1,10 +1,12 @@
 import time
 import asyncio
 
-async def greeting(message):
+async def greetings(message):
     for i in range(6):
         print(message)
         await asyncio.sleep(1)
 
 start_time = time.time()
 
+asyncio.run(greetings("Hello"))
+asyncio.run(greetings("World"))
