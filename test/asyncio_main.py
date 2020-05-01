92939a6eb9b975a1a84840683f9d00771346ab59
Liberbell <liberbell@gmail.com>
2020-05-01 14:07:59 +0900

    asyncio_main2

--- test/asyncio_main.py
+++ test/asyncio_main.py
@@ -1,2 +1,10 @@
 import asyncio
 import time
+
+async def greetings(message):
+    for i in range(6):
+        print(message)
+        await asyncio.sleep(1)
+
+async def main():
+    start_time = time.time()
