ce8306ab30def35955b9de2be524a62b4f563737
Liberbell <liberbell@gmail.com>
2020-05-01 14:20:56 +0900

    asyncio_gather4

--- test/asyncio_gather.py
+++ test/asyncio_gather.py
@@ -5,3 +5,8 @@ async def greetings(message):
     for i in range(6):
         print(message)
         await asyncio.sleep(1)
+
+async def print_numbers(num):
+    for i in range(num):
+        print(i)
+        await asyncio.sleep(1)
